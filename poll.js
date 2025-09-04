406d0b49021c8dbb0cbcde8630310846c04c0b52
Punith <punithv.pv@gmail.com>
2025-09-04 09:16:00 +0530

    Corrected - Delivery Resheduled By Customer

--- poll.js
+++ poll.js
@@ -279,7 +279,7 @@ const interpretStatus = (t) => {
   if (instructions.includes('maximum attempts reached')) return 'IN - TRANSIT';
   if (instructions.includes('package missing in audit')) return 'IN - TRANSIT';
   if (instructions.includes('package found in audit')) return 'IN - TRANSIT';
-  if (instructions.includes('celivery rescheduled by customer')) return 'IN - TRANSIT';
+  if (instructions.includes('delivery rescheduled by customer')) return 'IN - TRANSIT';
   if (instructions.includes('delayed due to weather conditions')) return 'IN - TRANSIT';
   if (instructions.includes('natural disaster')) return 'IN - TRANSIT';
   if (instructions.includes('ntd updated')) return 'RTO IN - TRANSIT';
